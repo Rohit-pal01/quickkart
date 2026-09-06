d1f356410a83f0683bcb59c2bba62316f75f0a47
Rohit <rohitpal98222@gmail.com>
2026-09-06 10:30:39 +0530

    Remove redundant root server.js

--- server.js
+++
@@ -1,5 +0,0 @@
-// Entry point for hosting platforms (like Render) running node server.js from root
-const path = require('path');
-require('dotenv').config({ path: path.resolve(__dirname, 'server', '.env') });
-require('dotenv').config();
-require('./server/server.js');
